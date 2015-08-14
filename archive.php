36586824c45b11ce8a045cd93061aa29bd84d8c6
Jesse Dillman <JDillman@bibleleague.org>
2015-08-14 10:44:51 -0500

    product pages

--- archive.php
+++ archive.php
@@ -16,46 +16,18 @@
 ?>
 <?php get_header(); ?>
 
-<body>
-	<div class="container animate">
+<h1><?php single_cat_title(); ?></h1>
 
-		<div class="content">
+<!--Start the Loop.-->
 
-			<div class="post-container">
-				<h1>Posts for category: <?php single_cat_title(); ?></h1>
-			</div>
+<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php
-			// set the "paged" parameter (use 'page' if the query is on a static front page)
-			$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+	<div class="product">
 
-			// the query
-			$the_query = new WP_Query( '&paged=' . $paged ); 
-			?>
-
-			<?php while ( have_posts() ) : the_post(); ?>
-			<div class="post-container">
-				<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
-
-				<div class="meta">
-
-					<p class="date"><em><?php the_date(); ?></em> | </p>
-					
-					<?php the_category(); ?>
-				</div>
-
-				<?php the_excerpt(); ?>
-			</div>
-			<?php endwhile; ?>
-
-			<p style="float:right;"><?php next_posts_link( 'Previous &raquo;', $the_query->max_num_pages );?></p>
-			<p style="float:left;"><?php previous_posts_link( '&laquo; Next' ); ?></p>
-		</div>
-
-		<div class="aside animate">
-			<?php get_sidebar(); ?>
-		</div>
+		<h2><?php the_title(); ?></h2>
+		
+		<?php the_content(); ?>
 	</div>
-</body>
+<?php endwhile; ?>
 
 <?php get_footer(); ?>
