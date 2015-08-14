071f8d249dda7cabb88eef8e69dbc5420f7e6ab5
Jesse Dillman <JDillman@bibleleague.org>
2015-08-14 11:36:34 -0500

    product pages

---
+++ category.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * If you'd like to further customize these archive views, you may create a
+ * new template file for each one. For example, tag.php (Tag archives),
+ * category.php (Category archives), author.php (Author archives), etc.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ */
+?>
+<?php get_header(); ?>
+
+<h1><?php single_cat_title(); ?></h1>
+<br>
+
+<!--Start the Loop.-->
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+	<div class="product">
+
+		<?php the_post_thumbnail(); ?> 
+
+		<h2><?php the_title(); ?></h2>
+
+		<?php the_content(); ?>
+	</div>
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
