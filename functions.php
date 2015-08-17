ce30a00109661546159f9ed00f759ccca4fa61be
Jesse Dillman <JDillman@bibleleague.org>
2015-08-17 12:20:08 -0500

    upload size

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
 
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
+
 function theme_setup() {
 
 	// Let WordPress manage the document title.
