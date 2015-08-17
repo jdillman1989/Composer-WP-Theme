7f4b8fa3a5de56363dc98c7456af60c54d914b97
Jesse Dillman <JDillman@bibleleague.org>
2015-08-17 11:33:02 -0500

    fix for prod footer

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 			<div class="footer-meta">
 
-				<?xml version="1.0" encoding="utf-8"?>
+				<?php echo '<?xml version="1.0" encoding="utf8"?>'; ?>
 				<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 				<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 					 viewBox="0 0 663.3 147.3" enable-background="new 0 0 663.3 147.3" xml:space="preserve">
